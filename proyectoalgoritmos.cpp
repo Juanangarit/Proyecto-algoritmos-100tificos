8063388d42af044ce22285d5c9e3a63fa3f65a0e
sebasangarit@gmail.com <sebasangarit@gmail.com>
2023-10-20 18:09:34 -0500

    primer vistazo del proyecto

---
+++ proyectoalgoritmos.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+// ... [Otros #include necesarios] ...
+
+// Definiciones y Estructuras de Datos
+struct Estudiante { /*...*/ };
+struct Edificio { /*...*/ };
+struct Nodo { /*...*/ };
+
+// Variables globales (si las hay)
+// ...
+
+// Funciones Auxiliares
+Nodo* insertar(Nodo* raiz, Edificio* e) { /*...*/ }
+Edificio* buscarEdificioCercano(Nodo* raiz, int capacidad) { /*...*/ }
+// ... [Otras funciones auxiliares] ...
+
+int main() {
+    // Recopilación de Datos
+    // ...
+
+    // Preprocesamiento
+    // ...
+
+    // Paso 1: Organización Inicial
+    // ...
+
+    // Paso 2: Asignación Inicial de Estudiantes
+    // ...
+
+    // Paso 3: Ajustes de Asignación
+    // ...
+
+    // Paso 4: Reasignación para Optimizar Espacio
+    // ...
+
+    // Paso 5: Revisión Final y Ajustes usando Árboles
+    // ...
+
+    // Paso 6: Salida y Verificación
+    // ...
+
+    // Limpieza y Cierre
+    // ...
+
+    return 0;
+}
+
